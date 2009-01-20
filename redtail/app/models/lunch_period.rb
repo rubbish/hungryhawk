b07bb7fa89f675803f1236f5cddbfb9609a4ced4
Luke Amdor <luke@absoluterubbish.net>
2009-01-19 22:44:09 -0600

    Added database annotation comments to models

--- redtail/app/models/lunch_period.rb
+++ redtail/app/models/lunch_period.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+# Schema version: 20090113051314
+#
+# Table name: lunch_periods
+#
+#  id              :integer(4)      not null, primary key
+#  period          :integer(4)
+#  name            :string(255)
+#  begin_end_times :string(255)
+#  to_go_available :boolean(1)
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class LunchPeriod < ActiveRecord::Base
 
   def to_s
--- redtail/app/models/reservation.rb
+++ redtail/app/models/reservation.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+# Schema version: 20090113051314
+#
+# Table name: reservations
+#
+#  id              :integer(4)      not null, primary key
+#  user_id         :integer(4)
+#  restaurant_id   :integer(4)
+#  lunch_period_id :integer(4)
+#  take_out        :string(255)
+#  drink           :string(255)
+#  dressing        :string(255)
+#  special_request :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class Reservation < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
